d7a1387c79e555e8a901678b56d7a0c6a4f76873
unknown <allie.crezel@gmail.com>
2021-03-19 15:36:58 +0100

    init

---
+++ bookmarks.js
@@ -0,0 +1,39 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Daily",
+    links: [
+      { name: "Drive", url: "https://drive.google.com" },
+      { name: "Pinterest", url: "https://www.pinterest.fr/" },
+      { name: "Tumblr", url: "https://www.tumblr.com/dashboard" },
+      { name: "GitHub", url: "https://github.com" },
+    ],
+  },
+  {
+    title: "Media",
+    links: [
+      { name: "Youtube", url: "https://youtube.com" },
+      { name: "9anime", url: "https://www14.9anime.to/home" },
+      { name: "Manga Fox", url: "http://fanfox.net/" },
+      { name: "Viz", url: "https://mangaplus.shueisha.co.jp/updates" },
+    ],
+  },
+  {
+    title: "Reddit",
+    links: [
+      { name: "/r/all", url: "https://reddit.com/r/all" },
+      { name: "/r/ProgrammerHumor/", url: "https://www.reddit.com/r/ProgrammerHumor/" },
+      { name: "/r/HumansBeingBros/", url: "https://www.reddit.com/r/HumansBeingBros/" },
+      { name: "/r/NintendoSwitch/", url: "https://www.reddit.com/r/NintendoSwitch/" },
+    ],
+  },
+  {
+    title: "Professional",
+    links: [
+      { name: "Inbox", url: "https://mail.google.com/mail/u/1/#inbox" },
+      { name: "Pole Emploi", url: "https://candidat.pole-emploi.fr/espacepersonnel/" },
+      { name: "NetyPareo", url: "http://netypareo.promeo-formation.fr/Net-YPareo/index.php" },
+      { name: "Easi", url: "https://connect.easi-training.fr/" },
+    ],
+  },
+];
