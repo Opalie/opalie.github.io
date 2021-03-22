0f501060764edd6427f304c190ce42f175b6d54e
unknown <allie.crezel@gmail.com>
2021-03-22 10:27:16 +0100

    update
    removed useless aseets
    added favicon
    added some neat JS

--- bookmarks.js
+++ bookmarks.js
@@ -1,39 +1,33 @@
 // Note: having length != 4 will mess with layout based on how the site is styled
+
 const bookmarks = [
   {
     title: "Daily",
     links: [
-      { name: "Drive", url: "https://drive.google.com" },
-      { name: "Pinterest", url: "https://www.pinterest.fr/" },
-      { name: "Tumblr", url: "https://www.tumblr.com/dashboard" },
-      { name: "GitHub", url: "https://github.com" },
+      { fa: "fab fa-google-drive", name: "Google Drive", url: "https://drive.google.com" },
+      { fa: "fas fa-file-alt", name: "Google Docs", url: "https://docs.google.com/document/u/0/" },
+      { fa: "fab fa-pinterest", name: "Pinterest", url: "https://www.pinterest.fr/" },
+      { fa: "fab fa-tumblr-square", name: "Tumblr", url: "https://www.tumblr.com/dashboard" },
+      { fa: "fab fa-github", name: "GitHub", url: "https://github.com" },
+      { fa: "fab fa-reddit", name: "/r/all", url: "https://reddit.com/r/all" },
     ],
   },
   {
     title: "Media",
     links: [
-      { name: "Youtube", url: "https://youtube.com" },
-      { name: "9anime", url: "https://www14.9anime.to/home" },
-      { name: "Manga Fox", url: "http://fanfox.net/" },
-      { name: "Viz", url: "https://mangaplus.shueisha.co.jp/updates" },
+      { fa: "fab fa-youtube", name: "Youtube", url: "https://youtube.com" },
+      { fa: "fas fa-play", name: "9anime", url: "https://www14.9anime.to/home" },
+      { fa: "fas fa-book", name: "Manga Fox", url: "http://fanfox.net/" },
+      { fa: "fab fa-reddit", name: "Viz", url: "https://mangaplus.shueisha.co.jp/updates" },
     ],
   },
   {
-    title: "Reddit",
+    title: "Pro",
     links: [
-      { name: "/r/all", url: "https://reddit.com/r/all" },
-      { name: "/r/ProgrammerHumor/", url: "https://www.reddit.com/r/ProgrammerHumor/" },
-      { name: "/r/HumansBeingBros/", url: "https://www.reddit.com/r/HumansBeingBros/" },
-      { name: "/r/NintendoSwitch/", url: "https://www.reddit.com/r/NintendoSwitch/" },
-    ],
-  },
-  {
-    title: "Professional",
-    links: [
-      { name: "Inbox", url: "https://mail.google.com/mail/u/1/#inbox" },
-      { name: "Pole Emploi", url: "https://candidat.pole-emploi.fr/espacepersonnel/" },
-      { name: "NetyPareo", url: "http://netypareo.promeo-formation.fr/Net-YPareo/index.php" },
-      { name: "Easi", url: "https://connect.easi-training.fr/" },
+      { fa: "fas fa-envelope-open-text", name: "Inbox", url: "https://mail.google.com/mail/u/1/#inbox" },
+      { fa: "fas fa-briefcase", name: "Pole Emploi", url: "https://candidat.pole-emploi.fr/espacepersonnel/" },
+      { fa: "fas fa-graduation-cap", name: "NetyPareo", url: "http://netypareo.promeo-formation.fr/Net-YPareo/index.php" },
+      { fa: "fas fa-ruler", name: "Easi", url: "https://connect.easi-training.fr/" },
     ],
   },
 ];
